d6a503732d74af313b40f846b9321449130ab554
vivianrcc <vivianrosanacc@gmail.com>
2024-01-22 17:46:04 -0300

    criei uma nova pasta e organizei os exercicios

---
+++ ExerciciosBootcamp/exercicio2.py
@@ -0,0 +1,5 @@
+#Faça um Programa que pergunte em que turno você estuda.
+#Peça para digitar M-matutino ou V-Vespertino ou N-Noturno.
+#Imprima a mensagem "BomDia!","BoaTarde!"ou"BoaNoite!"ou"ValorInválido!"
+
+turnoM= input ('Digite o turno que voce estuda ')  
